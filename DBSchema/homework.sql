e40de89bc90d60d98a2ee8c45c229998abd4d552
admin <kelvin9149@gmail.com>
2022-01-19 22:36:04 +0900

    hotfix filetype

--- DBSchema/homework.sql
+++ DBSchema/homework.sql
@@ -10,7 +10,7 @@ CREATE TABLE `homework` (
   `hw_posted` DATETIME DEFAULT current_timestamp,
   `hw_updated` DATETIME DEFAULT current_timestamp ON UPDATE current_timestamp,
   `hw_author` VARCHAR(13) NOT NULL,
-  `hw_file` timestamp NOT NULL DEFAULT current_timestamp,
+  `hw_file` LONGBLOB,
   PRIMARY KEY (`hw_id`),
   FOREIGN KEY (`studyroom_id`)
   REFERENCES studyroom(`study_id`) ON UPDATE CASCADE
