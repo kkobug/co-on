e310a706f07990702326d924d315705d612dbf07
김윤지 <yoonjigim0.0@gmail.com>
2022-01-20 04:53:20 +0000

    Update frontend/package-lock.json, backend/src/main/java/com/ssafy/api/controller/UserController.java files

--- backend/src/main/java/com/ssafy/api/controller/UserController.java
+++ backend/src/main/java/com/ssafy/api/controller/UserController.java
@@ -159,15 +159,15 @@ public class UserController {
 	})
 	public ResponseEntity<StudentRes> getStudentInfo(@ApiIgnore Authentication authentication) {
 		/**
-		 * public ResponseEntity<StudentRes> getStudentInfo(@ApiIgnore Authentication authentication) {
+
 		 * 요청 헤더 액세스 토큰이 포함된 경우에만 실행되는 인증 처리이후, 리턴되는 인증 정보 객체(authentication) 통해서 요청한 유저 식별.
 		 * 액세스 토큰이 없이 요청하는 경우, 403 에러({"error": "Forbidden", "message": "Access Denied"}) 발생.
 		 */
 		SsafyStudentDetails studentDetails = (SsafyStudentDetails)authentication.getDetails();
 		String studentId =studentDetails.getUsername();
-		System.out.println(studentId);
+		
 		Student student = studentService.findById(studentId);
-		System.out.println(student.getStEmail());
+
 		return ResponseEntity.status(200).body(StudentRes.of(student));
 	}
 
