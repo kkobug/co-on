b1ac5780deebc0848a8dfefdaf1141d47522fcbb
lee seung hun <bgd02013@gmail.com>
2022-02-08 21:18:08 +0900

    feat/과제관리

--- frontend/src/views/teacher/homework_manage.vue
+++ frontend/src/views/teacher/homework_manage.vue
@@ -1,13 +1,24 @@
 <template>
-  <tchr-nav @startvideo="start"></tchr-nav>
-  <h1>
-    과제 관리 페이지
-  </h1>
+  <div>
 
-  <start-video-dialog
-      :open="videoDialogOpen"
-      @closeVideoDialog="end"
-  ></start-video-dialog>
+    <tchr-nav @startvideo="start"></tchr-nav>
+    <h1>
+      과제 관리 페이지
+    </h1>
+    <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+      <el-tab-pane v-for="Hw in tableData" :key="Hw" :label="Hw.date" name="first">1</el-tab-pane>
+    </el-tabs>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="Date" width="180" />
+      <el-table-column prop="name" label="Name" width="180" />
+      <el-table-column prop="address" label="Address" />
+    </el-table>
+
+    <start-video-dialog
+        :open="videoDialogOpen"
+        @closeVideoDialog="end"
+    ></start-video-dialog>
+  </div>
 </template>
 
 <script>
@@ -29,7 +40,77 @@ export default {
     }
   },
   setup() {
-
+    const store = useStore()
+    const Hws = undefined
+    const getHw = function () {
+      store.dispatch('root/requestTchrListHomework',this.userId)
+      .then(result =>{
+        this.Hws=result.data
+        console.log(this.Hws)
+      })
+      .catch(function(err){
+        alert(err)
+      })
+    }
+    const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-08',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-06',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-07',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+]
+    return {tableData,Hws,getHw}
   },
   methods:{
     start (){
@@ -38,12 +119,20 @@ export default {
     },
     end (){
       this.videoDialogOpen= false
-    }
+    },
+
+  },
+  created: function(){
+    const localvuex=JSON.parse(localStorage.getItem('vuex'))
+    this.userId=localvuex["root"]["userid"]
+    this.getHw()
   }
 
 }
 </script>
 
 <style>
-
+#hide_table{
+  background-color: blueviolet;
+}
 </style>
