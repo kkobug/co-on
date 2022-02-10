7cafc6e203d51629a070a9a35c18e645dff4ef5c
lee seung hun <bgd02013@gmail.com>
2022-02-10 10:14:15 +0900

    end

--- frontend/src/views/teacher/homework_manage.vue
+++ frontend/src/views/teacher/homework_manage.vue
@@ -2,15 +2,35 @@
   <div>
     <tchr-nav @startvideo="start"></tchr-nav>
     <h1>과제 관리 페이지</h1>
-    <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-      <el-tab-pane v-for="Hw in tableData" :key="Hw" :label="Hw.date" :name="Hw.Date">{{Hw}}</el-tab-pane>
+    <el-tabs v-model="activeName" class="demo-tabs">
+      <el-tab-pane label="User" name="first">User</el-tab-pane>
+      <el-tab-pane v-for="Hw in Hws" :key="Hw" :label="Hw.hwTitle">
+        <!-- {{Hw.studentHomeworks[0].stHwFile[0]}} -->
+        <el-scrollbar height="400px">
+          <el-row>
+            <el-col :span="1"><div class="grid-content bg-head">번호</div></el-col>
+            <el-col :span="3"><div class="grid-content bg-head">ID</div></el-col>
+            <el-col :span="6"><div class="grid-content bg-head">내용</div></el-col>
+            <el-col :span="14"><div class="grid-content bg-head">파일명</div></el-col>
+          </el-row>
+          <el-row v-for="(item,index) in Hw.studentHomeworks" :key="index" >
+            <el-col :span="1"><div class="grid-content bg-content">{{index+1}}</div></el-col>
+            <el-col :span="3"><div class="grid-content bg-content">{{item.stId}}</div></el-col>
+            <el-col :span="6"><div class="grid-content bg-content">{{item.stHwcontent}}</div></el-col>
+            <el-col :span="12">
+              <div class="grid-content bg-content" v-if="item.stHwFile[0]">
+                {{item.stHwFile[0].fileOriginName}}
+              </div>
+              <div class="grid-content bg-content" v-else>
+                파일이 없습니다
+              </div>
+            </el-col>
+            <el-col :span="1"><el-input class="grid-content bg-content"/></el-col>
+            <el-col :span="1"><el-button @click="ent">D</el-button></el-col>
+          </el-row>
+        </el-scrollbar>
+      </el-tab-pane>
     </el-tabs>
-
-    <el-table :data="tableData" style="width: 100%">
-      <el-table-column prop="date" label="Date" width="180" />
-      <el-table-column prop="name" label="Name" width="180" />
-      <el-table-column prop="address" label="Address" />
-    </el-table>
 
     <start-video-dialog
         :open="videoDialogOpen"
@@ -26,12 +46,11 @@ import { ref, unref, reactive, computed, onMounted } from 'vue'
 
 import Tchr_nav from './tchr_nav.vue'
 import StartVideoDialog from './startvideo-dialog.vue'
-
 export default {
   name: 'Tchr_HWManage',
   components: {
     "tchr-nav": Tchr_nav,
-    StartVideoDialog
+    StartVideoDialog,
   },
   data(props){
     return {
@@ -40,69 +59,82 @@ export default {
   },
   setup() {
     const store = useStore()
-    const Hws = undefined
+    const Hws = []
     const activeName = ref('')
     const getHw = function () {
       store.dispatch('root/requestTchrListHomework',this.userId)
       .then(result =>{
         this.Hws=result.data
         console.log(this.Hws)
-        this.Hws.forEach(function(item,index,arr2){
-          console.log(item,index,arr2[index+1]);
-        })
       })
       .catch(function(err){
         alert(err)
       })
     }
-    const tableData = [
-  {
-    date: '2016-05-03',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-  },
-  {
-    date: '2016-05-02',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-  },
-  {
-    date: '2016-05-04',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-  }
-]
-    return {tableData,Hws,activeName, getHw}
+    const ent = function(a){
+      console.log(a)
+    }
+    return {Hws, activeName, getHw, ent}
   },
   methods:{
-    start (){
-      this.videoDialogOpen= true
-    },
-    end (){
-      this.videoDialogOpen= false
-    },
-
+    start (){this.videoDialogOpen= true},
+    end (){this.videoDialogOpen= false},
   },
   created: function(){
     const localvuex=JSON.parse(localStorage.getItem('vuex'))
     this.userId=localvuex["root"]["userid"]
     this.getHw()
-  }
+  },
 
 }
 </script>
 
-<style>
-#hide_table{
-  background-color: blueviolet;
+<style scoped>
+.el-row {
+  margin: 4px;
+  /* border: solid;
+  border-bottom-width: thick 2px;
+  border-color: #3D6657; */
+
+}
+.el-row:last-child {
+  margin-bottom: 0;
+}
+.el-col {
+  padding: 2px;
+
+  border-radius: 4px;
+}
+.bg-head {
+  background: #3D6657;
+  color: white;
+}
+.bg-content {
+  background: #BFEDD9;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+.scrollbar-demo-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  margin: 10px;
+  text-align: center;
+  border-radius: 4px;
+  background: var(--el-color-primary-light-9);
+  color: var(--el-color-primary);
 }
 </style>
